4fa431d359f0e0b73a24960aed3a7b60cd65905c
ashirbadsahu <ashirbadsahu300@gmail.com>
2023-09-02 19:44:30 +0530

    basic initial setup for bot

---
+++ README.md
@@ -0,0 +1,2 @@
+## Svelte help - a telegram bot for svelte resources
+It's a telegram bot made using python.
---
+++ Telegram.py
@@ -0,0 +1,21 @@
+import telegram.ext
+
+Token = "6466483620:AAEKWIsizoVuayz7rprof04uT0LdbaNdv7w"
+
+updater = telegram.ext.updater('6466483620:AAEKWIsizoVuayz7rprof04uT0LdbaNdv7w', useContext = True)
+dispatcher = updater.dispatcher
+
+def start(update, context):
+    update.message.reply_text('Hello welcome to Svelte help.')
+
+def help(update, context):
+    update.message.reply_text(
+        '''
+        /start -> Welcome to Svelte help!
+        /help -> See all list of commands
+        /beginner -> Get free resources for beginners
+        /uilib -> UI libraries for svelte
+        /channels -> Useful Youtube channels related on svelte
+        /deploy -> Deploy your project
+        '''
+    )
